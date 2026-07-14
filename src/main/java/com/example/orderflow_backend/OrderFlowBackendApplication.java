e54d795ea39804846c4f77c0a1743bc078d66b0d
akkhera37 <akkhera37@gmail.com>
2026-07-14 17:41:39 +0530

    First commit

---
+++ src/main/java/com/example/orderflow_backend/OrderFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFlowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderflow_backend/OrderFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
